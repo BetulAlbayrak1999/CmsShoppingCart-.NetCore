13fb6610b76aa6a1345a8a85581245f5832bad21
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-08-23 23:19:21 +0300

    Add project files.

---
+++ CmsShoppingCartMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CmsShoppingCartMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
