ffc1657ae2d200575a717dc9497e3042a814d927
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-08-31 14:03:29 +0300

    fix component error

---
+++ CmsShoppingCartMVC/ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogic.Services.CategoryServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCartMVC.ViewComponents
+{
+    public class CategoriesViewComponent: ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            try
+            {
+                var viewPageList = await _categoryService.GetAllAsync();
+                if (viewPageList != null)
+                    return View(viewPageList);
+                return View(null);
+
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+    }
+}
