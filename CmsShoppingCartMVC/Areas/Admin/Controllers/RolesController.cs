bf532e3f2fe0c292168d62b72b419545fca4a2bf
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-09-05 13:36:30 +0300

    adding RolesController using Identity

---
+++ CmsShoppingCartMVC/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using Entity.Domains;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCartMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()=> View(_roleManager.Roles);
+    }
+}
