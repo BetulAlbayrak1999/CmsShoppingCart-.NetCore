248951753d79c35c6cc5c6937e808961037e9ef8
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-09-05 14:31:11 +0300

    add Create a Role

--- CmsShoppingCartMVC/Areas/Admin/Controllers/RolesController.cs
+++ CmsShoppingCartMVC/Areas/Admin/Controllers/RolesController.cs
@@ -1,6 +1,9 @@
 ﻿using Entity.Domains;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace CmsShoppingCartMVC.Areas.Admin.Controllers
 {
@@ -16,5 +19,39 @@ namespace CmsShoppingCartMVC.Areas.Admin.Controllers
             _userManager = userManager;
         }
         public IActionResult Index()=> View(_roleManager.Roles);
+
+        // GET /admin/roles/create
+        public IActionResult Create() => View();
+
+
+        // POST /admin/roles/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([MinLength(2), Required] string name)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                    if (result.Succeeded)
+                    {
+                        TempData["Success"] = "The role has been created!";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (IdentityError error in result.Errors) ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+                ModelState.AddModelError("", "Minimum length is 2");
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
