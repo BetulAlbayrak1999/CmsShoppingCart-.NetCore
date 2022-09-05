f157bec961cbc29f52a4600cf2950ef394a3272d
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-09-05 19:37:44 +0300

    Create RolesTagHelper.cs

---
+++ BusinessLogic/Helpers/RolesTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using Entity.Domains;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Helpers
+{
+[HtmlTargetElement("td", Attributes = "user-role")]
+public class RolesTagHelper : TagHelper
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<AppUser> _userManager;
+
+    public RolesTagHelper(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+    {
+        _roleManager = roleManager;
+        _userManager = userManager;
+    }
+
+    [HtmlAttributeName("user-role")]
+    public string RoleId { get; set; }
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        List<string> names = new List<string>();
+        IdentityRole role = await _roleManager.FindByIdAsync(RoleId);
+
+        if (role != null)
+        {
+            foreach (var user in _userManager.Users)
+            {
+                if (user != null && await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    names.Add(user.UserName);
+                }
+            }
+        }
+
+        output.Content.SetContent(names.Count == 0 ? "No users" : string.Join(", ", names));
+    }
+}
+}
+
